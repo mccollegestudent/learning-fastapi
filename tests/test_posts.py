89ded39869f0f877e9ac5238c9b49dfe5a87e286
Manatsa Chiomadzi <94277576+mccollegestudent@users.noreply.github.com>
2023-11-23 18:13:49 -0500

    added another way to do validaton in test_get_all_posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -3,8 +3,13 @@ from app import schemas
 
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
-    for obj in res.json():
-        post_res = schemas.PostOut(**obj)
-        
+    # for obj in res.json():
+    #     post_res = schemas.PostOut(**obj)
+    def validate(post):
+        return schemas.PostOut(**post)
+    posts_map = map(validate, res.json())
+    posts_list = list(posts_map)
+           
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
+    
