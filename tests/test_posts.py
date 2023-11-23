281d330980302cf10470d6dff9e337dc531fceac
Manatsa Chiomadzi <94277576+mccollegestudent@users.noreply.github.com>
2023-11-23 16:11:53 -0500

    added test all posts + pydantic schema validation

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,10 @@
+from typing import List
+from app import schemas
+
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
-    print(res.json())
+    for obj in res.json():
+        post_res = schemas.PostOut(**obj)
+        
+    assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
